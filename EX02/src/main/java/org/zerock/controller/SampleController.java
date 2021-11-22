047ed66795ec29e1c5bf89a98af656dc2d3fe236
akakss225 <akakss225@naver.com>
2021-11-22 10:53:57 +0900

    REST방식 JSON / XML로 출력

--- EX02/src/main/java/org/zerock/controller/SampleController.java
+++ EX02/src/main/java/org/zerock/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,4 +19,11 @@ public class SampleController {
 		return "안녕하세요";
 		
 	}
+	
+	// return 값이 VO일경우 produces를 생략 가능 >> 기본값이 JSON과 XML이다.
+	@GetMapping(value = "/getSample" )
+	public SampleVO getSample() {
+		return new SampleVO(113, "rocket", "racoon");
+	}
+	
 }
