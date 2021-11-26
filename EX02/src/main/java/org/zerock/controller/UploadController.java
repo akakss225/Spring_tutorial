67f11aa4d933ac961572bf959cec575407a433a2
akakss225 <akakss225@naver.com>
2021-11-26 12:06:23 +0900

    파일 업로드_썸네일 만들기

--- EX02/src/main/java/org/zerock/controller/UploadController.java
+++ EX02/src/main/java/org/zerock/controller/UploadController.java
@@ -1,17 +1,28 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -49,12 +60,15 @@ public class UploadController {
 	@GetMapping("/uploadAjax")
 	public void uploadAjax() {}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile, Model model) {
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		String uploadFolder = "/Users/sumin/Desktop/Spring/upload";
+		List<AttachFileDTO> list = new ArrayList<>();
 		
+		String uploadFolderPath = getFolder();
 		// make folder-------------
-		File uploadPath = new File(uploadFolder, getFolder());
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("Upload Path : " + uploadPath);
 		
 		if(uploadPath.exists() == false) {
@@ -69,27 +83,59 @@ public class UploadController {
 			log.info("---------------------------------------------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
-		
+			
+			AttachFileDTO attachDTO = new AttachFileDTO();
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			log.info("Only File Name : " + uploadFileName.substring(uploadFileName.indexOf("/") + 1));
+			attachDTO.setFileName(uploadFileName);
 			
 			UUID uuid = UUID.randomUUID();
 			
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
-			File saveFile = new File(uploadPath, uploadFileName);
+			
 			
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
+				
 				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				if(checkImageType(saveFile)) {
+					attachDTO.setImage(true);
+					
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					// 썸네일을 만들어주는 메소드 createThumbnail()
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail,100,100);
+					
+					thumbnail.close();
+				}
 			}
 			catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
+	
+	private boolean checkImageType(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			return contentType.startsWith("image");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 	
 	
 }
